d6bfd12bf0be3361152074f35d7c933281847cdc
Jon Redeker <jon.redeker@gmail.com>
2018-04-04 17:15:22 -0400

    create team and player dashboards, using mlb stats example

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 # IMPORTS
 import psycopg2
 import yaml
+import json
 
 from flask import Flask, render_template
 
@@ -12,14 +13,27 @@ config_file = open("config.yml", "r")
 config = yaml.load(config_file)
 db = config['database_cfg']
 
+# data placeholder
+team_data = json.load(open("team_data.json"))
+player_data = json.load(open("player_data.json"))
+
 # placeholder just deal with it
-measures = [
-    {
-        'name': 'Code Coverage',
-        'overall': 50,
-        'recent': 90
-    }
-]
+
+
+def get_team_name(team):
+    this_team_name = team_data[team]['name']
+    return this_team_name
+
+
+def get_players(team):
+    this_team_data = team_data[team]['players']
+    return this_team_data
+
+
+def get_player_stats(player):
+    this_player_stats = player_data[player]
+    return this_player_stats
+
 
 # CONTROLLERS
 @app.route("/")
@@ -27,9 +41,27 @@ measures = [
 def index():
     return render_template('pages/index.html', title="Home", header="Home")
 
-@app.route("/dashboard")
-def dashboard():
-    return render_template('pages/dashboard.html', title="Dashboard", header="Dashboard", measures=measures)
+
+@app.route("/team_dashboard/<team>")
+def team_dashboard(team):
+    team_name = get_team_name(team)
+    players = get_players(team)
+    link_data = {
+        "sub_link_title": "",
+        "sub_link_pre": "/player_dashboard/",
+        "sub_links": players
+    }
+    return render_template('pages/team_dashboard.html', title=team_name,
+                           team_name=team_name, players=players,
+                           link_data=link_data)
+
+
+@app.route("/player_dashboard/<player>")
+def player_dashboard(player):
+    player_stats = get_player_stats(player)
+    print(player_stats)
+    return render_template('pages/player_dashboard.html', title="Dashboard",
+                           header="Dashboard", stats=player_stats)
 
 
 @app.route("/dbtest")
