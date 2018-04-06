d64483034c40769e18413835796d2eb0c8a75f44
Jon Redeker <jon.redeker@gmail.com>
2018-04-06 13:06:23 -0400

    add non nullables

--- app/models.py
+++ app/models.py
@@ -8,17 +8,21 @@ class Team(db.Model):
     __tablename__ = 'teams'
 
     team_id = db.Column(db.String(60), primary_key=True)
-    name = db.Column(db.String(120), unique=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    players = db.relationship('Player')
 
     def __init__(self, name):
         self.name = name
+
+    def __repr__(self):
+        return '<Team %r>' % self.name
 
 
 class Player(db.Model):
     __tablename__ = 'players'
 
     player_id = db.Column(db.String(60), primary_key=True)
-    name = db.Column(db.String(120))
+    name = db.Column(db.String(120), nullable=False)
     team_id = db.Column(db.String(60))
 
     def __init__(self, name, team_id):
@@ -30,9 +34,9 @@ class Statistic(db.Model):
     __tablename__ = 'statistics'
 
     stat_id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(60), unique=True)
+    name = db.Column(db.String(60), unique=True, nullable=False)
     position_type = db.Column(db.String(20))
-    positive = db.Column(db.BOOLEAN())
+    positive = db.Column(db.Boolean())
 
     def __init__(self, name, position_type, positive):
         self.name = name
@@ -44,7 +48,7 @@ class PlayerStat(db.Model):
     __tablename__ = 'player_stats'
 
     id = db.Column(db.Integer, primary_key=True)
-    stat_id = db.Column(db.Integer)
+    stat_id = db.Column(db.Integer, nullable=False)
     player_id = db.Column(db.String(60))
     career = db.Column(db.Integer)
     recent = db.Column(db.Integer)
